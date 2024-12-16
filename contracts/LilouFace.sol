c5ac85c66dc6bb6de9a7c25f14e423517c39c869
aaudelin <aurelien.audelin@gmail.com>
2024-12-16 17:56:04 +0100

    feat: add nft impl

---
+++ contracts/LilouFace.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "./IERC721.sol";
+
+contract LilouFace is ERC721 {
+    error InvalidAddress(address account);
+    error InvalidNft(uint256 tokenId);
+    error UnauthorizedAddress(address owner, address operator, uint256 tokenId);
+
+    mapping(address owner => uint256 tokensCount) private _balances;
+    mapping(uint256 tokenId => address owner) private _owners;
+    mapping(uint256 tokenId => address approval) private _approvals;
+    mapping(address owner => mapping(address operator => bool status)) private _operators;
+
+    constructor() {
+        
+    }
+
+    function balanceOf(address _owner) external view returns (uint256) {
+        if (_owner == address(0)) {
+            revert InvalidAddress(_owner);
+        }
+        return _balances[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        address owner = _owners[_tokenId];
+        if (owner == address(0)) {
+            revert InvalidNft(_tokenId);
+        }
+        return owner;
+    } 
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+        if (_to == address(0)) {
+            revert InvalidAddress(_to);
+        }
+        if (_owners[_tokenId] == address(0)) {
+            revert InvalidNft(_tokenId);
+        }
+        if (_from != _owners[_tokenId]
+            && msg.sender != _owners[_tokenId] 
+            && msg.sender != _approvals[_tokenId] 
+            && _operators[_from][msg.sender] == false
+        ) {
+            revert UnauthorizedAddress(_from, msg.sender, _tokenId);
+        }
+
+        _balances[_from] -= 1;
+        _balances[_to] += 1;
+        
+        _owners[_tokenId] = _to;
+
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function approve(address _approved, uint256 _tokenId) external payable {
+        if (_approved == address(0)) {
+            revert InvalidAddress(_approved);
+        }
+
+        address owner = _owners[_tokenId];
+        if (msg.sender != owner
+            && _operators[owner][msg.sender] == false
+        ) {
+            revert UnauthorizedAddress(owner, msg.sender, _tokenId);
+        }
+        _approvals[_tokenId] = _approved;
+
+        emit Approval(owner, _approved, _tokenId);
+    }
+
+    function setApprovalForAll(address _operator, bool _approved) external {
+        if (_operator == address(0)) {
+            revert InvalidAddress(_operator);
+        }
+
+        _operators[msg.sender][_operator] = _approved;
+
+        emit ApprovalForAll(msg.sender, _operator, _approved);
+
+    }
+
+    function getApproved(uint256 _tokenId) external view returns (address) {
+        if (_owners[_tokenId] == address(0)) {
+            revert InvalidNft(_tokenId);
+        }
+
+        return _approvals[_tokenId];
+    }
+
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool) {
+        return _operators[_owner][_operator];
+    }
+
+
+}
